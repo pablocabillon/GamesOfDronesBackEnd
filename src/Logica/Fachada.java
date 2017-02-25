8c9a68701aaa891c24f1479b65496b79cc4d3103
pablocabillon <pablocabillon@users.noreply.github.com>
2017-02-24 23:14:41 -0300

    Fachada actualizacion 24-2 solo contiene los metodos

--- src/Logica/Fachada.java
+++ src/Logica/Fachada.java
@@ -1,4 +1,4 @@
-
+import java.util.Hashtable;
 
 /*********************************************
  * Descripción: 	Facha de acceso a la CapaLogica que contiene los funciones generales
@@ -10,7 +10,45 @@
 public class Fachada {
 	
 	private Partida partida;
+
+	
+	 
+	public void crearPartida(int cantJugadores,Escenario vEscenario)
+	{
+			
+	}
 	
 	
+	public void unirseAPartida(int IdEquipo, String nombreJugador)
+	{
+		
+	}
+	
+	//Deberia estar la rotacion y angulo en la clase Objeto
+	public void movioDrone(int idObjeto, int coordX, int coordY, int rotacion, int angulo)
+	{
+		
+	}
+	
+	public void disparoBomba(int idObjeto)
+	{
+		
+	}
+	
+	//No se que devolver
+//	public void resibioDisparoCanion(int idObjeto, )
+//	{
+//
+//	}
+	
+	public void resibioDisparoBomba(int idObjeto)
+	{
+		
+	}
+	
+	public void guardarPartida()
+	{
+		
+	}
 
 }
