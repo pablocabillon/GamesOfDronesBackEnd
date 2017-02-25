b4e306800ff939d47d63a15ea8bdccac2dcc351c
pablocabillon <pablocabillon@users.noreply.github.com>
2017-02-24 22:33:10 -0300

    Primera version de la facha hecha por Juan

---
+++ src/Logica/Fachada.java
@@ -0,0 +1,16 @@
+
+
+/*********************************************
+ * Descripción: 	Facha de acceso a la CapaLogica que contiene los funciones generales
+ * Autor: 			Juan Fourment
+ * Versión: 		1.0.0
+ * Modificaciones: 
+ **********************************************/
+
+public class Fachada {
+	
+	private Partida partida;
+	
+	
+
+}
